28d89b17d4715362a5ce2e2b65c06311b958ab9c
nXqd <nxqd.inbox+rhacker@gmail.com>
2015-03-29 14:25:00 +0700

    update LAMP provisioner

--- linux/provision.sh
+++ linux/provision.sh
@@ -1,3 +1,9 @@
+# Locale
+locale-gen en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 # Latest gdb
 http://askubuntu.com/questions/529781/upgrade-from-gdb-7-7-to-7-8
 required packages: texinfo
@@ -16,10 +22,21 @@ https://gist.github.com/Globegitter/662713f90d5af5b4269d
 cd /tmp
 git clone git://github.com/joyent/node.git
 cd node
-./configure --prefix=~/local
+./configure
 make install
 cd ..
+
 
 git clone git://github.com/isaacs/npm.git
 cd npm
 make install # or `make link` for bleeding edge
+
+#nodejs must install packges
+npm install -g csslint
+
+
+# Javascript flow
+http://flowtype.org/docs/getting-started.html
+
+# LAMP
+https://gist.github.com/danpette/a4f9cd207f669f99b882
